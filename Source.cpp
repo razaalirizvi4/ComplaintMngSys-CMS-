bfd2b18a92e13be6e70bcd215d716eec45c98885
razaalirizvi4 <123810899+razaalirizvi4@users.noreply.github.com>
2024-02-11 01:20:31 +0500

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+#include <fstream>
+int main() {
+	string choice = "";
+	for (int i = 0; ; i++) {
+		cout << "Would you like to lodge a complaint: ";
+		cin >> choice;
+		cin.ignore();
+		if (choice == "yes") {
+			ofstream out("out.txt", ios::app);
+			ifstream read("compNum.txt");
+			char arr[1000];
+			char* ptr = nullptr;
+			ptr = new char[50];
+			cout << "Enter your email: ";
+			cin.getline(ptr, 50);
+			cout << "Lodge your complaint: ";
+			cin.getline(arr, 1000);
+			int number;
+			read >> number;
+			read.close();
+			cout << "Thank you for lodging your complaint, your time is very valuable to us. We will get back to you as soon as possible on " << ptr << "\nRegards, Joe Mama\nyour complaint id is " << number << endl;
+			delete[] ptr;
+			ptr = nullptr;
+			out << number << ". " << arr << endl;
+			ofstream write("compNum.txt");
+			write << number + 1;
+			write.close();
+			out.close();
+		}
+		else if (choice == "no") {
+			return 0;
+		}
+		else {
+			cout << "I couldnt Understand, please answer in yes or no: ";
+			cin >> choice;
+		}
+	}
+}
